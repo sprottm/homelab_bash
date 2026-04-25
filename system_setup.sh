da524aeaad6c0684848bc2cd915414bce5df5347
tuxott <tuxott@pm.me>
2026-04-25 15:48:27 -0400

    Improve documentation and how variables are passed in

--- system_setup.sh
+++ system_setup.sh
@@ -3,6 +3,29 @@
 # Used for setting up my Arch Linux system from the liveinstall
 # Not using Archinstall because I want more customizability
 
+# Display script usage and exit
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+function display_usage()
+{
+  echo "Usage: ${0} [OPTIONS]"
+    echo ''
+
+    local FORMAT="  %-3s  %-6s  %-18s  %-5s  %s\n"
+
+    printf "\033[1m${FORMAT}\033[0m" "Req" "Short" "Long" "Input" "Description"
+    echo ''
+
+    printf "${FORMAT}" "*" "-d" "--drive" "Yes" "Define drive to install Arch Linux onto (i.e. /dev/sda)"
+    printf "${FORMAT}" "" "-g" "--volume-group" "Yes" "Define name of volume group (default = vgroot)"
+    printf "${FORMAT}" "" "" "--log-level" "Yes" "Define log level for messages (default = info)"
+    printf "${FORMAT}" "" "-h" "--help" "No" "Display usage and exit"
+}
+
 # Source in the common functions before running the script
 # Globals:
 #   None
@@ -44,25 +67,53 @@ function source_dependencies()
 #   PRIMARY_DRIVE
 function define_variables()
 {
-  # TODO: Add input validation and make inputs not rely on position
-  PRIMARY_DRIVE="${1}"
-  VOLUME_GROUP="vgroot" 
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -d | --drive)
+        if is_block_device "${2}"; then
+          PRIMARY_DRIVE="${2}"
+          shift
+        else
+          return 1
+        fi
+      ;;
+      -g | --volume-group )
+        volume_group="${2}"
+        shift
+      ;;
+      --log-level)
+        USER_LOG_LEVEL="${2}"
+        shift
+      ;;
+      -h | --help)
+        display_usage
+        exit 0
+      ;;
+      *)
+        custom_log "error" "Unknown option: ${1}"
+        return 1
+      ;;
+    esac
+    shift
+  done 
+
+  [[ -z "${VOLUME_GROUP}" ]] && VOLUME_GROUP="vgroot"
 
   # Check if required variables were defined
   local readonly required_variables=( PRIMARY_DRIVE VOLUME_GROUP )
   check_required_variables "${required_variables[@]}"
   local return_code=$?
   if [[ "${return_code}" -ne 0 ]]; then
-    custom_log "fatal" "Required variables are not set!"
-    exit "${return_code}"
+    custom_log "error" "Required variables are not set!"
+    return "${return_code}"
   fi
 }
 
 source_dependencies
-define_variables ${*}
-is_network_up || return 1
-is_root || return 1
-is_block_device || return 1
+define_variables ${*} || exit 1
+is_network_up || exit 1
+is_root || exit 1
+is_block_device || exit 1
 create_partitions "${PRIMARY_DRIVE}"
 format_partitions "${PRIMARY_DRIVE}" "${VOLUME_GROUP}"
 set_mirrors
